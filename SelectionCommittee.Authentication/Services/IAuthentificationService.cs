f345aaab882becf20a9081381f0130aa88210963
errant_soul <kirianenko.vladislav@gmail.com>
2019-03-09 20:33:36 +0200

    Implemented authentification services commit

---
+++ SelectionCommittee.Authentication/Services/IAuthentificationService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace SelectionCommittee.Authentication.Services
+{
+    public interface IAuthentificationService
+    {
+        Task<bool> LogIn(string email, string password);
+
+        Task LogOut();
+
+        Task<IEnumerable<IdentityError>> Register(string email, string password, string confirmPassword,
+            params string[] roles);
+    }
+}
