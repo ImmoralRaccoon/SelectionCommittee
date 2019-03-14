768a5c1b57054608116b8b17df12a7eec2544d5e
errant_soul <kirianenko.vladislav@gmail.com>
2019-03-14 13:57:29 +0200

    Seeded Db with according data commit.

--- SelectionCommittee.API/Startup.cs
+++ SelectionCommittee.API/Startup.cs
@@ -40,7 +40,7 @@ namespace SelectionCommittee.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("EBANAYAdb-PIZDEC")));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SelectionCommitteeApi")));
             services.AddDbContext<IdentityContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SelectionCommitteeAuthentification")));
             services.AddMvc();
 
--- SelectionCommittee.DAL/EF/DatabaseSeeder.cs
+++ SelectionCommittee.DAL/EF/DatabaseSeeder.cs
@@ -7,11 +7,109 @@ namespace SelectionCommittee.DAL.EF
     {
         public static void Seed(ModelBuilder modelBuilder)
         {
+            // Faculties
+            modelBuilder.Entity<Faculty>().HasData(
+                new Faculty
+                {
+                    Id = 1,
+                    Name = "Faculty of Economics",
+                    NumberOfPlaces = 4,
+                    NumberOfBudgetPlaces = 2
+                },
+                new Faculty
+                {
+                    Id = 2,
+                    Name = "Faculty of Physics and Mathematics",
+                    NumberOfPlaces = 3,
+                    NumberOfBudgetPlaces = 1
+                },
+                new Faculty
+                {
+                    Id = 3,
+                    Name = "Faculty of History",
+                    NumberOfPlaces = 5,
+                    NumberOfBudgetPlaces = 3
+                },
+                new Faculty
+                {
+                    Id = 4,
+                    Name = "Faculty of Computer Science",
+                    NumberOfPlaces = 6,
+                    NumberOfBudgetPlaces = 5
+                },
+                new Faculty
+                {
+                    Id = 5,
+                    Name = "Faculty of Geology",
+                    NumberOfPlaces = 3,
+                    NumberOfBudgetPlaces = 1
+                });
+
+            // Enrollees
+            modelBuilder.Entity<Enrollee>().HasData(
+                new Enrollee
+                {
+                    Id = 1,
+                    FirstName = "Kirianenko",
+                    LastName = "Vladislav",
+                    Patronymic = "Igorevich",
+                    Email = "kirianenko.vladislav@gmail.com",
+                    Region = "Kiev",
+                    City = "Kiev",
+                    SchoolLyceumName = "Ukrainian Humanities Lyceum"
+                },
+                new Enrollee
+                {
+                    Id = 2,
+                    FirstName = "Pidkopay",
+                    LastName = "Ivan",
+                    Patronymic = "Vitalievich",
+                    Email = "pidkop@gmail.com",
+                    Region = "Kharkiv",
+                    City = "Kupiansk",
+                    SchoolLyceumName = "School №1"
+                },
+                new Enrollee
+                {
+                    Id = 3,
+                    FirstName = "Nesterenko",
+                    LastName = "Oleksiy",
+                    Patronymic = "Genadievich",
+                    Email = "alex@gmail.com",
+                    Region = "Kiev",
+                    City = "Kiev",
+                    SchoolLyceumName = "School №123"
+                },
+                new Enrollee
+                {
+                    Id = 4,
+                    FirstName = "Sologubchenko",
+                    LastName = "Ivanka",
+                    Patronymic = "Egorovna",
+                    Email = "sologor@gmail.com",
+                    Region = "Dnipro",
+                    City = "Kamenskoe",
+                    SchoolLyceumName = "Lyceum №33"
+                },
+                new Enrollee
+                {
+                    Id = 5,
+                    FirstName = "Stadchenko",
+                    LastName = "Marianna",
+                    Patronymic = "Igorevna",
+                    Email = "marishka@gmail.com",
+                    Region = "Kharkiv",
+                    City = "Kharkiv",
+                    SchoolLyceumName = "School №12"
+                }
+                );
+
+            // Assessments
             modelBuilder.Entity<Assessment>().HasData(
                 new Assessment
                 {
                     Id = 1,
-                    Name = "Math",
+                    Name = "Ukrainian language",
                     Grade = 10,
                     GradeType = "school",
                     EnrolleeId = 1
@@ -19,7 +117,7 @@ namespace SelectionCommittee.DAL.EF
                 new Assessment
                 {
                     Id = 2,
-                    Name = "Ukrainian language",
+                    Name = "Math",
                     Grade = 12,
                     GradeType = "school",
                     EnrolleeId = 1
@@ -27,7 +125,7 @@ namespace SelectionCommittee.DAL.EF
                 new Assessment
                 {
                     Id = 3,
-                    Name = "English",
+                    Name = "English language",
                     Grade = 11,
                     GradeType = "school",
                     EnrolleeId = 1
@@ -35,75 +133,302 @@ namespace SelectionCommittee.DAL.EF
                 new Assessment
                 {
                     Id = 4,
-                    Name = "Ukrainian language",
+                    Name = "Geography",
                     Grade = 12,
-                    GradeType = "exam",
+                    GradeType = "shool",
                     EnrolleeId = 1
                 },
                 new Assessment
                 {
                     Id = 5,
+                    Name = "History of Ukraine",
+                    Grade = 10,
+                    GradeType = "school",
+                    EnrolleeId = 1
+                },
+                new Assessment
+                {
+                    Id = 6,
+                    Name = "Ukrainian language",
+                    Grade = 11,
+                    GradeType = "exam",
+                    EnrolleeId = 1
+                },
+                new Assessment
+                {
+                    Id = 7,
                     Name = "Math",
+                    Grade = 11,
+                    GradeType = "exam",
+                    EnrolleeId = 1
+                },
+                new Assessment
+                {
+                    Id = 8,
+                    Name = "Geography",
+                    Grade = 12,
+                    GradeType = "exam",
+                    EnrolleeId = 1
+                },
+
+                new Assessment
+                {
+                    Id = 9,
+                    Name = "Ukrainian language",
                     Grade = 10,
                     GradeType = "school",
                     EnrolleeId = 2
                 },
                 new Assessment
                 {
-                    Id = 6,
+                    Id = 10,
+                    Name = "Math",
+                    Grade = 9,
+                    GradeType = "school",
+                    EnrolleeId = 2
+                },
+                new Assessment
+                {
+                    Id = 11,
+                    Name = "English language",
+                    Grade = 11,
+                    GradeType = "school",
+                    EnrolleeId = 2
+                },
+                new Assessment
+                {
+                    Id = 12,
+                    Name = "Geography",
+                    Grade = 7,
+                    GradeType = "shool",
+                    EnrolleeId = 2
+                },
+                new Assessment
+                {
+                    Id = 13,
+                    Name = "History of Ukraine",
+                    Grade = 8,
+                    GradeType = "school",
+                    EnrolleeId = 2
+                },
+                new Assessment
+                {
+                    Id = 14,
+                    Name = "Ukrainian language",
+                    Grade = 10,
+                    GradeType = "exam",
+                    EnrolleeId = 2
+                },
+                new Assessment
+                {
+                    Id = 15,
+                    Name = "Math",
+                    Grade = 10,
+                    GradeType = "exam",
+                    EnrolleeId = 2
+                },
+                new Assessment
+                {
+                    Id = 16,
+                    Name = "Physics",
+                    Grade = 7,
+                    GradeType = "exam",
+                    EnrolleeId = 2
+                },
+
+                new Assessment
+                {
+                    Id = 17,
                     Name = "Ukrainian language",
                     Grade = 5,
                     GradeType = "school",
-                    EnrolleeId = 2
+                    EnrolleeId = 3
                 },
                 new Assessment
                 {
-                    Id = 7,
-                    Name = "English",
-                    Grade = 3,
-                    GradeType = "school",
-                    EnrolleeId = 2
-                },
-                new Assessment
-                {
-                    Id = 8,
-                    Name = "Ukrainian language",
+                    Id = 18,
+                    Name = "Math",
                     Grade = 9,
-                    GradeType = "exam",
-                    EnrolleeId = 2
-                });
-
-            modelBuilder.Entity<Enrollee>().HasData(
-                new Enrollee
-                {
-                    Id = 1,
-                    FirstName = "Clad",
-                    LastName = "Dyihcic",
-                    Patronymic = "Notingem",
-                    Email = "vdyi@gmai.com",
-                    Region = "Kharkiv",
-                    City = "Kharkiv",
-                    SchoolLyceumName = "Shool #12"
+                    GradeType = "school",
+                    EnrolleeId = 3
                 },
-                new Enrollee
+                new Assessment
                 {
-                    Id = 2,
-                    FirstName = "Stolyarenko",
-                    LastName = "Vladislav",
-                    Patronymic = "Notingem",
-                    Email = "vdy23i@gmai.com",
-                    Region = "Kupiansk",
-                    City = "Kharkiv",
-                    SchoolLyceumName = "Shool #2"
-                });
+                    Id = 19,
+                    Name = "English language",
+                    Grade = 11,
+                    GradeType = "school",
+                    EnrolleeId = 3
+                },
+                new Assessment
+                {
+                    Id = 20,
+                    Name = "Geography",
+                    Grade = 7,
+                    GradeType = "shool",
+                    EnrolleeId = 3
+                },
+                new Assessment
+                {
+                    Id = 21,
+                    Name = "History of Ukraine",
+                    Grade = 12,
+                    GradeType = "school",
+                    EnrolleeId = 3
+                },
+                new Assessment
+                {
+                    Id = 22,
+                    Name = "Ukrainian language",
+                    Grade = 10,
+                    GradeType = "exam",
+                    EnrolleeId = 3
+                },
+                new Assessment
+                {
+                    Id = 23,
+                    Name = "History",
+                    Grade = 5,
+                    GradeType = "exam",
+                    EnrolleeId = 3
+                },
+                new Assessment
+                {
+                    Id = 24,
+                    Name = "English language",
+                    Grade = 7,
+                    GradeType = "exam",
+                    EnrolleeId = 3
+                },
 
-            modelBuilder.Entity<Faculty>().HasData(
-                new Faculty
+                new Assessment
                 {
-                    Id = 1,
-                    Name = "Economy",
-                    NumberOfPlaces = 123,
-                    NumberOfBudgetPlaces = 23
+                    Id = 25,
+                    Name = "Ukrainian language",
+                    Grade = 10,
+                    GradeType = "school",
+                    EnrolleeId = 4
+                },
+                new Assessment
+                {
+                    Id = 26,
+                    Name = "Math",
+                    Grade = 12,
+                    GradeType = "school",
+                    EnrolleeId = 4
+                },
+                new Assessment
+                {
+                    Id = 27,
+                    Name = "English language",
+                    Grade = 11,
+                    GradeType = "school",
+                    EnrolleeId = 4
+                },
+                new Assessment
+                {
+                    Id = 28,
+                    Name = "Geography",
+                    Grade = 7,
+                    GradeType = "shool",
+                    EnrolleeId = 4
+                },
+                new Assessment
+                {
+                    Id = 29,
+                    Name = "History of Ukraine",
+                    Grade = 8,
+                    GradeType = "school",
+                    EnrolleeId = 4
+                },
+                new Assessment
+                {
+                    Id = 30,
+                    Name = "Ukrainian language",
+                    Grade = 10,
+                    GradeType = "exam",
+                    EnrolleeId = 4
+                },
+                new Assessment
+                {
+                    Id = 31,
+                    Name = "Math",
+                    Grade = 6,
+                    GradeType = "exam",
+                    EnrolleeId = 4
+                },
+                new Assessment
+                {
+                    Id = 32,
+                    Name = "English language",
+                    Grade = 7,
+                    GradeType = "exam",
+                    EnrolleeId = 4
+                },
+
+                new Assessment
+                {
+                    Id = 33,
+                    Name = "Ukrainian language",
+                    Grade = 11,
+                    GradeType = "school",
+                    EnrolleeId = 5
+                },
+                new Assessment
+                {
+                    Id = 34,
+                    Name = "Math",
+                    Grade = 11,
+                    GradeType = "school",
+                    EnrolleeId = 5
+                },
+                new Assessment
+                {
+                    Id = 35,
+                    Name = "English language",
+                    Grade = 12,
+                    GradeType = "school",
+                    EnrolleeId = 5
+                },
+                new Assessment
+                {
+                    Id = 36,
+                    Name = "Geography",
+                    Grade = 12,
+                    GradeType = "shool",
+                    EnrolleeId = 5
+                },
+                new Assessment
+                {
+                    Id = 37,
+                    Name = "History of Ukraine",
+                    Grade = 10,
+                    GradeType = "school",
+                    EnrolleeId = 5
+                },
+                new Assessment
+                {
+                    Id = 38,
+                    Name = "Ukrainian language",
+                    Grade = 11,
+                    GradeType = "exam",
+                    EnrolleeId = 5
+                },
+                new Assessment
+                {
+                    Id = 39,
+                    Name = "English language",
+                    Grade = 11,
+                    GradeType = "exam",
+                    EnrolleeId = 5
+                },
+                new Assessment
+                {
+                    Id = 40,
+                    Name = "Geography",
+                    Grade = 12,
+                    GradeType = "exam",
+                    EnrolleeId = 5
                 });
         }
     }
